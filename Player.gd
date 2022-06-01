1547c3ccb03848780a8031f0724510b1e13a8fd9
2easy2do <enriquealexy@gmail.com>
2022-06-01 08:33:56 -0500

    Barrel Interaction system in place

--- Player.gd
+++ Player.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 export var speed := 700
 var drink = "none"
+var can_interact = false
+var new_drink
+
 func _physics_process(_delta:float) -> void:
 	var input_vector:= Vector2(
 		Input.get_action_strength("RIGHT") - Input.get_action_strength("LEFT"),
@@ -9,3 +12,17 @@ func _physics_process(_delta:float) -> void:
 	)
 	var move_direction := input_vector.normalized()
 	move_and_slide(speed * move_direction)
+	
+func _on_Area2D_body_entered(body):
+	if body.name == "Barrel":
+		can_interact = true
+		new_drink = body.drink
+		
+func _on_Area2D_body_exited(body):
+	can_interact = false
+	new_drink = null
+	
+func _input(event):
+	if Input.is_action_pressed("ACTION") && can_interact == true:
+		drink = new_drink
+		print(drink)
--- Tavern/Barrel.gd
+++ Tavern/Barrel.gd
@@ -1,22 +1,19 @@
 extends StaticBody2D
 
 var drink = "Beer"
-var close_to_barrel = false
-var get_drink = false
+var can_interact = false
+var player
 
-func _process(delta):
-	if close_to_barrel == true:
-		if Input.is_action_pressed("ACTION"):
-				get_drink =  true
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		$Label.text = drink
 		$Label.visible = true
-		close_to_barrel = true
-		if get_drink == true:
-			body.drink = drink
+		can_interact = true
 		print(body.drink)
+		body = player
 
 func _on_Area2D_body_exited(body):
 	yield(get_tree().create_timer(0.5), "timeout")
 	$Label.visible = false
+	
+
