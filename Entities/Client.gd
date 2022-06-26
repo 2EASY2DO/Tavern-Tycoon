ac4140d13e548eea0345e875aa78f620fcc6f0ed
tooeasy2do <enriquealexy@gmail.com>
2022-06-25 21:01:53 -0500

    FInally made the AI work based oin a taken or not taken seat

--- Entities/Client.gd
+++ Entities/Client.gd
@@ -7,15 +7,17 @@ var chair = null
 var path : Array = []
 var velocity = Vector2.ZERO
 var speed = 250
+var walking = false
+
 #Change the customer graphics randomly every time they spawn
 func _ready():
 	_decide_drink()
+	get_closest_seat()
 	var tree = get_tree()
 	print(tree)
 	if tree.has_group("navigation"):
 		print("Found it")
 		level_navigation = tree.get_nodes_in_group("navigation")[0]
-	
 # Just here so i can cahnge the drink they want for debug purposes
 func _process(delta):
 	if Input.is_action_just_pressed("RELOAD"):
@@ -25,12 +27,17 @@ func _physics_process(delta):
 	if chair and level_navigation:
 		generate_path()
 		navigate()
+		walking = true
 	move()
 	
 func move():
+	if chair and chair.free == false:
+		get_closest_seat()
 	velocity = move_and_slide(velocity)
 	
 func _on_Area2D_body_entered(body):
+	if body == chair:
+		body.free = false
 	if body.name == "Player":
 		if body.drink == drink:
 			$Label.text = "Thanks"
@@ -53,23 +60,21 @@ func _decide_drink():
 func navigate():
 	if path.size() > 0:
 		velocity =  global_position.direction_to(path[1]) * speed
-		print("navigating path")
 		
 		if global_position == path[0]:
 			path.pop_front()
 			
 func generate_path():
-	print("Generated Path")
 	if level_navigation != null and chair != null:
 		path = level_navigation.get_simple_path(global_position, chair.global_position, false)
 
-func _on_seat_area_body_entered(body):
-	print("looking")
-	var min_dist = 9999
-	if body.is_in_group("seats"):
-		print("Found a seat")
-		var dist = self.position.distance_to(body.position)
-		if (dist < min_dist):
+func get_closest_seat():
+	var min_dist = 99999
+	var min_seat
+	var seats = get_tree().get_nodes_in_group("seats")
+	for seat in seats:
+		var dist = self.position.distance_to(seat.position)
+		if (dist < min_dist and seat.free == true):
 			min_dist = dist
-			chair = body
-	return chair
+			min_seat = seat
+	chair = min_seat
---
+++ Tavern/Chair.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+var free = true
+
+func _ready():
+	pass # Replace with function body.
